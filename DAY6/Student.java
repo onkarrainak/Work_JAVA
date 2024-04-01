1998f45d330f9c34b393e6cf3bc32328c053baa8
onkarrainak <rainakonkar@gmail.com>
2024-04-01 19:21:14 +0530

    add file

---
+++ DAY6/Student.java
@@ -0,0 +1,20 @@
+public class Student {
+    static String cname = "JAVA";
+
+public static void findArea()
+{
+	System.out.println("This is static method ");
+	
+	System.out.println(cname + " Static variable calling");
+	
+}
+	
+public static void main(String[] args) 
+
+{
+	
+	Student.findArea();
+	
+}
+    
+}
